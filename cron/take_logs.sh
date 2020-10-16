095ed0155804dc96ed0cafc99f83eedff3b78f55
ilyaro <40502115+ilyaro@users.noreply.github.com>
2020-10-16 22:20:43 +0300

    Update take_logs.sh

--- cron/take_logs.sh
+++ cron/take_logs.sh
@@ -4,11 +4,13 @@
 # Author: Ilya Rokhkin
 
 ## Standard name of lightstreamer container
-container_name=ls-server
-
+container_name="ls-server"
+date=`date`
+log_file="/logs/ls-docker.log"
+echo "Log of $container_name of $date" > $log_file
 if [ "$( docker container inspect -f '{{.State.Running}}' $container_name )" == "true" ]; 
 then
-  echo $container_name is running> /logs/ls-docker.log
+  echo $container_name "is running" >> $log_file
 else
-  
+  echo $container_name "is not running" >> $log_file 
 fi
